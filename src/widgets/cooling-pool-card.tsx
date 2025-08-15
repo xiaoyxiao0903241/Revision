9508ffd1d1c82674fe995a6acec05be95f3796bf
hht <hht@163.com>
2025-08-15 09:00:57 +0800

    fix: 冷却池卡片移动端布局

--- src/widgets/cooling-pool-card.tsx
+++ src/widgets/cooling-pool-card.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
-import { useTranslations } from 'next-intl';
-import { FC } from 'react';
-import { Button, View } from '~/components';
-import { cn } from '~/lib/utils';
-import { usePublicClient } from 'wagmi';
-import { useUserAddress } from '~/contexts/UserAddressContext';
-import { toast } from 'sonner';
-import YielodLockAbi from '~/wallet/constants/YielodLockAbi.json';
 import { useQueryClient } from '@tanstack/react-query';
-import { useWriteContractWithGasBuffer } from '~/hooks/useWriteContractWithGasBuffer';
-import { yieldLocker } from '~/wallet/constants/tokens';
+import { useTranslations } from 'next-intl';
+import { FC, useState } from 'react';
+import { toast } from 'sonner';
 import { Abi } from 'viem';
+import { usePublicClient } from 'wagmi';
+import { Button, View } from '~/components';
+import { useUserAddress } from '~/contexts/UserAddressContext';
+import { useWriteContractWithGasBuffer } from '~/hooks/useWriteContractWithGasBuffer';
+import { cn } from '~/lib/utils';
+import { yieldLocker } from '~/wallet/constants/tokens';
+import YielodLockAbi from '~/wallet/constants/YielodLockAbi.json';
 
 interface CoolingPoolCardProps {
   data: {
@@ -91,46 +90,58 @@ export const CoolingPoolCard: FC<CoolingPoolCardProps> = ({
       clipDirection='topLeft-bottomRight'
       clipSize={16}
     >
-      <div className={cn('flex flex-col items-center')}>
+      <div className={cn('flex gap-2 md:flex-col items-center')}>
         {/* 动画齿轮图标 */}
-        <div
-          className={cn('w-2/3 aspect-square flex items-center justify-center')}
-        >
-          {children}
-        </div>
-
-        {/* 已释放数量 */}
-        <div className={cn('text-2xl font-bold font-mono', data.className)}>
-          {data.claimable}
-        </div>
-        <div className='text-xs text-foreground/50'>{t('released')}</div>
-
-        {/* 等待释放数量 */}
-        <div className='flex w-full items-center justify-between my-2'>
-          <div className='text-xs text-foreground/50'>
-            {t('waitingToBeReleased')}
-          </div>
-          <div className='text-xs font-mono text-white'>
-            {data.waiting} ({data.waitingPercent.toFixed(2)}%)
-          </div>
-        </div>
-        {/* 进度条 */}
-        <div className='bg-foreground/20 w-full h-2 rounded-full overflow-hidden'>
+        <div className='flex flex-col w-1/3 md:w-2/3 items-center'>
           <div
-            className={cn('h-full relative overflow-hidden', data.bgClassName)}
-            style={{
-              width: `${data.waitingPercent}%`,
-            }}
+            className={cn(
+              'w-full  aspect-square flex items-center justify-center p-4'
+            )}
           >
-            {/* 斜纹效果 */}
+            {children}
+          </div>
+
+          {/* 已释放数量 */}
+          <div
+            className={cn(
+              'text-base md:text-2xl font-bold font-mono',
+              data.className
+            )}
+          >
+            {data.claimable}
+          </div>
+          <div className='text-xs text-foreground/50'>{t('released')}</div>
+        </div>
+        <div className='flex-1'>
+          {/* 等待释放数量 */}
+          <div className='flex w-full items-center justify-between my-2'>
+            <div className='text-xs text-foreground/50'>
+              {t('waitingToBeReleased')}
+            </div>
+            <div className='text-xs font-mono text-white'>
+              {data.waiting} ({data.waitingPercent.toFixed(2)}%)
+            </div>
+          </div>
+          {/* 进度条 */}
+          <div className='bg-foreground/20 w-full h-2 rounded-full overflow-hidden'>
             <div
               className={cn(
-                'absolute inset-0',
-                data.active && 'animate-stripes'
+                'h-full relative overflow-hidden',
+                data.bgClassName
               )}
               style={{
-                width: 'calc(100% + 8px)',
-                backgroundImage: `
+                width: `${data.waitingPercent}%`,
+              }}
+            >
+              {/* 斜纹效果 */}
+              <div
+                className={cn(
+                  'absolute inset-0',
+                  data.active && 'animate-stripes'
+                )}
+                style={{
+                  width: 'calc(100% + 8px)',
+                  backgroundImage: `
                    repeating-linear-gradient(
                      45deg,
                      rgba(255, 255, 255, 0.1) 0px,
@@ -139,28 +150,29 @@ export const CoolingPoolCard: FC<CoolingPoolCardProps> = ({
                      transparent 2px
                    )
                  `,
-              }}
-            />
+                }}
+              />
+            </div>
           </div>
-        </div>
-        {/* 周期 */}
-        <div className='flex w-full items-center justify-between my-4'>
-          <div className='text-xs text-foreground/50'>{t('period')}</div>
-          <div className='text-xs font-mono text-white'>
-            {data.period}
-            {tStaking('days')}
+          {/* 周期 */}
+          <div className='flex w-full items-center justify-between my-4'>
+            <div className='text-xs text-foreground/50'>{t('period')}</div>
+            <div className='text-xs font-mono text-white'>
+              {data.period}
+              {tStaking('days')}
+            </div>
           </div>
-        </div>
 
-        {/* 领取按钮 */}
-        <Button
-          className='w-full'
-          disabled={data.disabled || isDisabled}
-          onClick={claimReward}
-          clipDirection='topLeft-bottomRight'
-        >
-          {current === data.period ? t2('claiming') : t2('claim')}
-        </Button>
+          {/* 领取按钮 */}
+          <Button
+            className='w-full h-8 md:h-12'
+            disabled={data.disabled || isDisabled}
+            onClick={claimReward}
+            clipDirection='topLeft-bottomRight'
+          >
+            {current === data.period ? t2('claiming') : t2('claim')}
+          </Button>
+        </div>
       </div>
     </View>
   );
